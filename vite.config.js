38d304e2aa0f19578d2c6f52118af1f2330d1a18
Johan Havenga <johan.havenga@gmail.com>
2025-02-03 14:09:35 +0200

    build test

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: [
+                'resources/css/app.css', 
+                'resources/css/bootstrap.css',
+                'resources/css/style.css', 
+                'resources/css/onepage.css', 
+                'resources/css/font-icons.css', 
+                'resources/css/custom.css', 
+                'resources/js/app.js'
+            ],
+            refresh: true,            
         }),
     ],
 });
